20a02fe4cc0b7b98234c814a40c78a72a762f048
Raine Virta <raine.virta@gmail.com>
2016-02-10 19:23:37 +0200

    enable babel for dependencies

--- scripts/000-es6-support.js
+++ scripts/000-es6-support.js
@@ -1,5 +1,7 @@
 // Description:
 //   Support scripts written in ES6
 
-require('babel-register')
+require('babel-register')({
+  ignore: false // enable babel for node_modules/hubot-ramda-docs
+})
 module.exports = () => {}
