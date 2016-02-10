745e6bc4bfe7ba940a798eccd7f350c75261d69a
Raine Virta <raine.virta@gmail.com>
2016-02-10 19:24:09 +0200

    add deployment related stuff

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rsync -avzr \
+  --exclude 'deploy.sh' \
+  --exclude '.git' \
+  --exclude 'node_modules' \
+  . ec2:ram-bot/
+
+ssh -t ec2 'cd ram-bot; npm install; pm2 startOrRestart app.json'
