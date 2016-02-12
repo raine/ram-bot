b3eb67d10dace32b60abd62d0e276df0de41d611
Raine Virta <raine.virta@gmail.com>
2016-02-13 00:29:51 +0200

    add upgrade-guides script

---
+++ scripts/upgrade-guides.js
@@ -0,0 +1,9 @@
+// Description:
+//   upgrade guides
+
+const { mdLink } = require('hubot-ramda-docs/src/markdown')
+const UPGRADE_GUIDES_ISSUES_URL = 'https://github.com/ramda/ramda/issues?q=is%3Aissue+label%3A%22upgrade+guide%22'
+
+module.exports = (bot) =>
+  bot.respond(/upgrade guides/, (res) =>
+    res.reply(mdLink('Upgrade guides', UPGRADE_GUIDES_ISSUES_URL)))
