88d01cf4db5df3e45bf22e9bf30a985413ded449
Raine Virta <raine.virta@gmail.com>
2016-02-15 11:26:20 +0200

    000-es6-support: only compile local scripts

--- scripts/000-es6-support.js
+++ scripts/000-es6-support.js
@@ -1,8 +1,5 @@
 // Description:
 //   Support scripts written in ES6
 
-require('babel-register')({
-  // TODO: compile hubot-ramda-docs and hubot-eval-js to ES5
-  only: /((hubot-ramda-docs|hubot-eval-js)\/src)|\/scripts/
-})
+require('babel-register')
 module.exports = () => {}
